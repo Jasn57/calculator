9f84df925c2b438b4bf5c1a92659c2147f916880
Jasn <jasn74939@gmail.com>
2025-03-27 17:41:48 -0400

    update all

--- script.js
+++ script.js
@@ -9,10 +9,10 @@ function clearDisplay() {
 function calculateResult() {
     let display = document.getElementById("display").value;
     try {
-        
+      
         document.getElementById("display").value = eval(display);
     } catch (error) {
-        
+       
         document.getElementById("display").value = "Error";
     }
 }
